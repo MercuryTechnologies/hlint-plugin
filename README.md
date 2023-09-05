2aa0febb8ac250aa926c60057c4ca42e3fa6b439
Gabriella Gonzalez <gabriella@mercury.com>
2023-09-05 10:43:57 -0500

    Add GHC plugin test (#2)
    This ensures that the plugin actually works as documented.

--- README.md
+++ README.md
@@ -14,3 +14,17 @@ The main advantages of doing this are:
 - You only have to lint modules that you rebuild
 
 - You don't have to parse the module twice
+
+To use this plugin, add this package as a build dependency and then enable the
+following GHC options (typically in the `ghc-options:` field of your `.cabal`
+file):
+
+```bash
+-fplugin HLint.Plugin
+```
+
+You can pass command-line options to `hlint` using `-fplugin-opt`, like this:
+
+```bash
+-fplugin HLint.Plugin -fplugin-opt='HLint.Plugin:--ignore=Redundant guard'
+```
--- flake.nix
+++ flake.nix
@@ -1,6 +1,5 @@
 { inputs = {
-    nixpkgs.url =
-      "github:NixOS/nixpkgs/cddebdb60de376c1bdb7a4e6ee3d98355453fe56";
+    nixpkgs.url = "github:NixOS/nixpkgs/23.05";
 
     utils.url = github:numtide/flake-utils;
   };
@@ -28,10 +27,25 @@
                           ghc90 = "3.3.6";
                           ghc92 = "3.4.1";
                           ghc94 = "3.5";
+                          # The following line is not actually used because the
+                          # Nixpkgs version we're on doesn't include
+                          # `hlint-3.6.1`, yet.  Instead, the actual
+                          # `hlint-3.6.1` build comes from `ghc96/hlint.nix`.
+                          # Once we upgrade to a new enough Nixpkgs we can
+                          # delete that file and then the following line will
+                          # kick in and work.
                           ghc96 = "3.6.1";
                         }."${compiler}";
 
-                        hlint-plugin = ./.;
+                        hlint-plugin =
+                          self.lib.cleanSourceWith
+                            { filter = name: type:
+                                    self.lib.cleanSourceFilter name type
+                                &&  !(self.lib.hasSuffix ".nix" name)
+                                &&  !(builtins.baseNameOf name == "README.md");
+
+                              src = ./.;
+                            };
                       })
 
                       (self.haskell.lib.packagesFromDirectory {
@@ -75,6 +89,41 @@
 
             program = "${pkgs.hlint-plugin}/bin/hlint-plugin";
           };
+
+         checks =
+           let
+             module = pkgs.writeText "Main.hs"
+               ''
+               {-# LANGUAGE OverloadedStrings #-}
+
+               main :: IO ()
+               main | otherwise = (mempty)
+               '';
+
+             expected = pkgs.writeText "expected.txt"
+               ''
+
+               Main.hs:4:20: warning:
+                   Redundant bracket
+                   Perhaps: mempty
+                 |
+               4 | main | otherwise = (mempty)
+                 |                    ^^^^^^^^
+               '';
+           in
+             pkgs.runCommand "hlint-plugin-${compiler}-test"
+               { nativeBuildInputs = [
+                   (pkgs.haskell.packages."${compiler}".ghcWithPackages
+                     (pkgs: [ pkgs.hlint-plugin ])
+                   )
+                 ];
+               }
+               ''
+               ln --symbolic ${module} Main.hs
+               ghc -fplugin HLint.Plugin -fplugin-opt='HLint.Plugin:--ignore=Redundant guard' -Wno-missed-extra-shared-lib -c Main.hs 2> >(tee actual.txt >&2)
+               diff ${expected} actual.txt
+               touch $out
+               '';
 
           devShells = pkgs.haskell.packages."${compiler}".hlint-plugin.env;
         }
