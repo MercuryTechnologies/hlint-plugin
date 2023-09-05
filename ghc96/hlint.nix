41fd616493bf872d828c2dea74e49467eea052af
Gabriella Gonzalez <gabriella@mercury.com>
2023-09-05 10:05:03 -0500

    Add GHC plugin test
    This ensures that the plugin actually works as documented.

---
+++ ghc96/hlint.nix
@@ -0,0 +1,27 @@
+{ mkDerivation, aeson, ansi-terminal, base, bytestring, cmdargs
+, containers, cpphs, data-default, deriving-aeson, directory, extra
+, file-embed, filepath, filepattern, ghc-lib-parser
+, ghc-lib-parser-ex, hscolour, lib, process, refact, text
+, transformers, uniplate, unordered-containers, utf8-string, vector
+, yaml
+}:
+mkDerivation {
+  pname = "hlint";
+  version = "3.6.1";
+  sha256 = "3280132bb3c1b39faa4c8c6a937479d4622484914da6a227a2ce4b15a76741fd";
+  isLibrary = true;
+  isExecutable = true;
+  enableSeparateDataOutput = true;
+  libraryHaskellDepends = [
+    aeson ansi-terminal base bytestring cmdargs containers cpphs
+    data-default deriving-aeson directory extra file-embed filepath
+    filepattern ghc-lib-parser ghc-lib-parser-ex hscolour process
+    refact text transformers uniplate unordered-containers utf8-string
+    vector yaml
+  ];
+  executableHaskellDepends = [ base ];
+  homepage = "https://github.com/ndmitchell/hlint#readme";
+  description = "Source code suggestions";
+  license = lib.licenses.bsd3;
+  mainProgram = "hlint";
+}
